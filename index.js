9debb215079cddac9cbbcf9621f8af7657e4fd76
nascimentodeborapro-pixel <nascimentodeborapro@gmail.com>
2026-02-28 11:40:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,53 @@
 const columns = document.querySelectorAll(".column_card");
 const cards = document.querySelectorAll(".card");
-const boarData =~[];
+const boarData =[];
 let draggedCard;
 
+/*  DRAG START   */ 
 
 const dragStart = (event) => {
-    draggedCard = event.target;
+    draggedCard = event.currentTarget;
     event.dataTransfer.effectallowed = "move";
+
+    // aaplica ao class la no CSS;
+    draggedCard.classList.add('dragging');
 };
+
+/*    DRAGOVER      */ 
+
+
 const dragOver = (event) => {
     event.preventDefault();
 };
+
+/*    DRAG ENTER  */ 
+
+
 const dragEnter = ({ target }) => {
     if(target.classList.contains("column_card")) {
         target.classList.add("column--highlitgh");
     }
 };
+/*    DRAG LEAVE*/
+
+
 const dragLeave = ({ target }) => {
     target.classList.remove("column--highlitgh");
 };
+
+/*    DROP  */
+
+
 const drop = ({ target }) => {
-    if(target.classList.contains("column_card")){
-     target.classList.remove("column--highlitgh");
-    target.append(draggedCard);
+    // quando drop passar o card para outra coluna 
+    if (target.classList.contains("column_card")) {
+        target.classList.remove("column--highlitgh");
+
+        const button = target.querySelector('.add-cardbtn');
+        if (draggedCard && draggedCard !== button) {
+            target.insertBefore(draggedCard, button);
+        }
     }
-   
 };
 
 
@@ -43,18 +66,24 @@ const creatCard = (column, button) => {
     });
 
     card.addEventListener("dragstart", dragStart);
-    card.addEventListener("dblclick", creatCard)
-    column.insertBefore(card, button)
-
-    column.insertBefore(card, button)
+    card.addEventListener("dblclick", creatCard);
+    ;
+    
+    column.insertBefore(card, button);    
     card.focus();
 
 }
  
 cards.forEach((card) => {
     card.addEventListener("dragstart", dragStart);
-});
+    card.addEventListener("dragend", (event) => {
 
+        draggedCard = null;
+    
+        event.currentTarget.classList.remove('dragging');
+    });
+});
+// aqui e onde a função esta sendo excutada
 columns.forEach((column) => {
     column.addEventListener("dragover", dragOver);
     column.addEventListener("dragenter", dragEnter);
@@ -66,6 +95,9 @@ columns.forEach((column) => {
 
     addBtn.addEventListener("click", () => {
         creatCard(column, addBtn);
+
     });
+
+
 });
 
